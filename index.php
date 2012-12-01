59dad60df66e339c1e82f0ed84e5765f812d925c
PengFei Zhang <blissfei@gmail.com>
2012-12-01 14:48:16 +0800

    add yii framework

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'./yii/framework/yii.php';
+//var_dump($yii);
+
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
